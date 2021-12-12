a62f3a092086ed1d1ad9d24e01687ec45978b459
SiddhartaDutta <siddhartadutta99@gmail.com>
2021-12-12 02:01:00 -0800

    Implementation of command files

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,5 +1,7 @@
 require('dotenv').config(); 
 
+const fs = require('fs');
+
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
@@ -9,12 +11,13 @@ clientId = process.env.CLIENT_ID;
 guildId = process.env.GUILD_ID;
 token = process.env.BRUHTHER_TOKEN;
 
-const commands = [
-	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-]
-	.map(command => command.toJSON());
+const commands = [];
+const commandFiles = fs.readdirSync('./Commands').filter(file => file.endsWith('.js'));
+
+for(const file of commandFiles){
+	const command = require(`./Commands/${file}`);
+	commands.push(command.data.toJSON());
+}
 
 const rest = new REST({ version: '9' }).setToken(token);
 
--- index.js
+++ index.js
@@ -1,24 +1,44 @@
 require('dotenv').config();     // Initialize dotenv
 
-const { Client, Intents } = require('discord.js');
+const fs = require('fs');
+
+const { Client, Collection, Intents } = require('discord.js');
 
 const Bruhther = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// .commands property addition to Bruhther
+Bruhther.commands = new Collection();
+
+// Filtered array of all file names in directory
+const commandFiles = fs.readdirSync('./Commands').filter(file => file.endsWith('.js'));
+
+for(const file of commandFiles){
+    const command = require(`./Commands/${file}`);
+    // Set a new item in the collection
+    // With the key as the command name and the value as the exported module
+    Bruhther.commands.set(command.data.name, command);
+}
 
 Bruhther.on('ready', () => {
     console.log('ready!');
 });
 
-Bruhther.on("debug", console.log);
+//Bruhther.on("debug", console.log);
 
 Bruhther.on('interactionCreate', async interaction => {
 
-    if(!interaction.isCommand()) return;
+    if (!interaction.isCommand()) return;
 
-    const {commandName} = interaction;
+	const command = client.commands.get(interaction.commandName);
 
-    if(commandName === 'ping'){
-        await interaction.reply('Pong!');
-    }
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
 
 });
 
